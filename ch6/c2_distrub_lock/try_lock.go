f6499dfc4e62a1056b8d43e92b8f69b3796b80a8
lvoooo <wuqize5109@163.com>
2021-02-07 01:06:29 +0800

    distrub_lock

---
+++ ch6/c2_distrub_lock/try_lock.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"sync"
+)
+
+type Lock struct {
+	c chan struct{}
+}
+
+func NewLock() Lock {
+	var l Lock
+	l.c = make(chan struct{}, 1)
+	l.c <- struct{}{}
+	return l
+}
+
+func (l Lock) Lock() bool {
+	lockResult := false
+	select {
+	case <-l.c:
+		lockResult = true
+	default:
+	}
+	return lockResult
+}
+
+func (l Lock) Unlock() {
+	l.c <- struct{}{}
+}
+
+var counter int
+
+func main() {
+	var l = NewLock()
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if !l.Lock() {
+				println("lock	failed")
+				return
+			}
+			counter++
+			println("current	counter", counter)
+			l.Unlock()
+		}()
+	}
+	wg.Wait()
+}
